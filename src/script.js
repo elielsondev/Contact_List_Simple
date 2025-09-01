d8fa89c192656f0c7dd940528de7e2ac255f59b6
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-09-01 04:47:43 -0300

    feat - interactive contact registration, listing and searching for contacts by partial or full name

--- src/script.js
+++ src/script.js
@@ -21,11 +21,80 @@ addContact(contactName, contactPhone, contactEmail);
 console.log(contacts[0]);
 
 // • Funcionalidades obrigatórias (menu interativo com prompt):
-// • Cadastrar contato (validar duplicados).
-// • Listar todos os contatos no console.
-// • Buscar contatos pelo nome (parcial ou completo).
-// • Atualizar contato existente.
-// • Remover contato da lista.
+menu = "";
+
+do {
+    menu = prompt(`
+        Escolha uma opção: 
+        1 - Adicionar, 
+        2 - Listar, 
+        3 - Buscar, 
+        4 - Atualizar, 
+        5 - Remover 
+        Ou digite "sair" para sair a qualquer momento.
+    `).toLowerCase();
+
+    switch (menu) {
+        case "1":
+        // • Cadastrar contato (validar duplicados).
+            let contactName = prompt("Digite o nome do contato:");
+            let contactPhone = prompt("Digite o telefone do contato:");
+            let contactEmail = prompt("Digite o email do contato:");
+
+            addContact(contactName, contactPhone, contactEmail);
+            break;
+    
+        case "2":
+            // • Listar todos os contatos no console.
+            for (let i = 0; i < contacts.length; i += 1) {
+                console.log(`
+                ------------------------
+                Contato ${i + 1}:
+                ------------------------
+                Nome: ${contacts[i][0]}
+                Telefone: ${contacts[i][1]}
+                E-mail: ${contacts[i][2]}
+                `);
+            };
+            console.log("Fim da lista.");
+            break;
+
+        case "3":
+            // • Buscar contatos pelo nome (parcial ou completo).
+            let searchName = prompt("Digite o nome do contato que deseja buscar:");
+            
+            for (let i = 0; i < contacts.length; i++) {
+                if (contacts[i][0].toLowerCase().includes(searchName.toLowerCase())) {
+                    console.log(`
+                    ------------------------
+                    Contato ${i + 1}:
+                    ------------------------
+                    Nome: ${contacts[i][0]}
+                    Telefone: ${contacts[i][1]}
+                    E-mail: ${contacts[i][2]}
+                    `);
+                }
+            };
+            break;
+
+        case "4":
+            // • Atualizar contato existente.
+            break;
+        
+        case "5":
+            // • Remover contato da lista.
+            break;
+
+        case "sair":
+            console.log("Saindo...");
+            break;
+
+        default:
+            console.log("Opção inválida, tente novamente.");
+            break;
+    }
+
+} while (menu !== "sair");
 
 // Agenda de Contatos
 // Simples
