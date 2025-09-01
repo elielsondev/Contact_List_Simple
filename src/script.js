06aec296649044cdca409cff4c5a00df60d46b80
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-08-31 23:08:28 -0300

    feat - adding project requirements as a comment

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
 console.log("Script loaded successfully!");
+
+// Objetivo: Criar uma agenda simples para armazenar contatos.
+
+// Requisitos:
+// • Cada contato deve ter: nome, telefone e email.
+// • Usar uma matriz para armazenar os contatos (cada linha = um
+// contato).
+// • Funcionalidades obrigatórias (menu interativo com prompt):
+// • Cadastrar contato (validar duplicados).
+// • Listar todos os contatos no console.
+// • Buscar contatos pelo nome (parcial ou completo).
+// • Atualizar contato existente.
+// • Remover contato da lista.
+
+// Agenda de Contatos
+// Simples
+
+// Conceitos usados:
+
+// •Arrays ou matrizes para guardar nome, telefone e email.
+// •Estruturas condicionais para buscar ou validar duplicatas.
+// •Repetições para listar todos os contatos.
+
+// Extra: permitir atualizar ou excluir um contato.
